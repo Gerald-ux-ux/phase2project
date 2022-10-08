6cab37d8e720ec0ae9f66089291d8ebd976a4427
gerald <you@example.com>
2022-10-08 16:00:18 +0300

    adding icons to the page

--- src/Components/Topbar.js
+++ src/Components/Topbar.js
@@ -5,10 +5,14 @@ import './Topbar.css'
 
 const Topbar = () => {
   return (
-    <div className='top'>
-      Topbar
+    <div className="top">
+      <div className="topLeft">
+        <i className="topIcon fab fa-instagram-square"></i>
+        <i class="fa-brands fa-youtube"></i>
+        <i className="topIcon fab fa-twitter-square"></i>
+      </div>
     </div>
-  )
+  );
 }
 
 export default Topbar
